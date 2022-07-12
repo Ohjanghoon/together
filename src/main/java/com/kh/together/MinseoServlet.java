55015335d501b5832c7dd741de2a44c390b7c7fb
qkralstj0808 <qkralstj08@gmail.com>
2022-07-12 14:35:33 +0900

    minseoServlet 추가! ilchanServlet 변경!

--- src/main/java/com/kh/together/MinseoServlet.java
+++ src/main/java/com/kh/together/MinseoServlet.java
@@ -8,20 +8,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class ilchanServlet
+ * Servlet implementation class MinseoServlet
  */
-@WebServlet("/ilchan")
-public class IlchanServlet extends HttpServlet {
+@WebServlet("/minseo")
+public class MinseoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("i hate git!");
-		response.getWriter().append("me too!!");
-		response.getWriter().append("haha");
+		response.getWriter().append("hi!");
 	}
 
 }
